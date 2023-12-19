1d35177df38e8ad7df68ae43d6b7244e79f5772b
mehdi-benhariz <benharizmehdi20@gmail.com>
2023-12-19 22:01:16 +0100

    update cv

--- src/containers/GithubProfile.jsx
+++ src/containers/GithubProfile.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, Suspense } from "react";
 import Loading from "../components/Loading";
 import { openSource } from "../portfolio";
-import { get } from "axios";
+import axios from "axios"; 
 import GithubProfileCard from "../components/GithubProfileCard";
 const GithubProfile = () => {
   const quary = `https://api.github.com/users/${openSource.githubUserName}`;
   const [prof, setProf] = useState({});
   async function getProfileData(q) {
-    await get(q)
+    await axios.get(q)
       .then((res) => setProf(res.data))
       .catch((err) => console.log(err));
   }
